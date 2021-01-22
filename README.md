3fbda15c740344db33a472c8a543b9294d902f20
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-21 19:07:26 -0500

    added screenshots and figlet

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 # Description
 A simple command-line application that allows non-developers to view and interact with information stored in databases.
+
+# Resources
+npm package to create ascii art text [figlet](https://www.npmjs.com/package/figlet)
+
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+//npm package for ascii text art
+const figlet = require('figlet');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -15,10 +17,20 @@ connection.connect((err) => {
     init();
 });
 
+//ascii text art
+figlet('Employee Manager', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
 const init = () => {
     inquirer.prompt([
         {
-            
+            name
         }
     ])
 }

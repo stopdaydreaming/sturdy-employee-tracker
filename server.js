0edd07da82eefd985e9cedf9bc0f048f09272747
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-21 19:59:36 -0500

    add file structure

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 //npm package for ascii text art
-const figlet = require('figlet');
+const figlet = require("figlet");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "HotTo$$y",
-    database:""
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "HotTo$$y",
+  database: "./db/employees_db"
 });
 
-connection.connect((err) => {
-    if(err) throw err;
-    console.log(`connected as id: ${connection.threadId}`);
-    init();
+connection.connect(err => {
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+  init();
 });
 
 //ascii text art
-figlet('Employee Manager', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
+figlet("Employee Manager", function(err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
 
-const init = () => {
-    // inquirer.prompt([
-    //     {
-    //         name
-    //     }
-    // ])
-}
+//init();
 
 const exit = () => {
-    connection.end();
-}
+  connection.end();
+};

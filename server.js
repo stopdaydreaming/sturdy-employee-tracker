d175d73813014a459e41c3a4a8cf6fa50cc8598b
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-22 19:47:27 -0500

    moved all code to one file

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-// const mysql = require("mysql");
-// const inquirer = require("inquirer");
-// const init = require("./lib/init");
-const connection = require("./db/connection");
-
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 //npm package for ascii text art
 const figlet = require("figlet");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "HotTo$$y",
+  database: "employees_db"
+});
+
+// connection.connect();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+  init();
+});
 
 //ascii text art
 figlet("Employee Manager", function(err, data) {
@@ -16,4 +28,92 @@ figlet("Employee Manager", function(err, data) {
   console.log(data);
 });
 
-module.exports = { connection };
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "View All Roles",
+          "Update Employee Roles",
+          "Exit"
+        ]
+      }
+    ])
+    .then(function(answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          viewAll();
+          break;
+
+        case "View All Employees By Department":
+          viewByDept();
+          break;
+
+        case "View All Employees By Manager":
+          viewByManager();
+          break;
+
+        case "Add Employee":
+          addEmp();
+          break;
+
+        case "Remove Employee":
+          removeEmp();
+          break;
+
+        case "Update Employee Role":
+          updEmpByRole();
+          break;
+
+        case "Update Employee Manager":
+          updEmpByManager();
+          break;
+
+        case "Exit":
+          exit();
+          break;
+      }
+    });
+};
+
+const viewAll = () => {
+  console.log("view all");
+  init();
+}
+
+const viewByDept = () => {
+  console.log("view by department");
+  IntersectionObserver();
+};
+
+const viewByManager = () => {
+  console.log("view by manager");
+};
+
+const addEmp = () => {
+  console.log("add employees");
+};
+
+const removeEmp = () => {
+  console.log("remove employee");
+};
+
+const updEmpByRole = () => {
+  console.log("update employee by role");
+};
+
+const exit = () => {
+  console.log("Goodbye!");
+  connection.end();
+};
+
